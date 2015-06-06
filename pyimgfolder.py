b45693e365b209ad697ee4fd66559669f5838dfe
Hartmut Seichter <hartmut@technotecture.com>
2015-06-07 00:41:41 +0200

    added copying

--- pyimgfolder.py
+++ pyimgfolder.py
@@ -5,6 +5,7 @@ import fnmatch
 import os
 import time
 import re
+import shutil
 
 
 from PIL import Image
@@ -13,7 +14,7 @@ from PIL.ExifTags import TAGS
 class ImageFolder(object):
     def __init__(self):
         self.matches = []
-        self.pathes = []
+        self.path = {}
 
     def is_image_file(self,filename, extensions=['.jpg', '.JPG', '.jpeg', '.gif', '.png']):
         return any(filename.endswith(e) for e in extensions)
@@ -31,35 +32,66 @@ class ImageFolder(object):
 
     def analyze(self):
         for m in self.matches:
-            img = Image.open(m)
-            exif_data = img._getexif()
+            datetime = None
+            try:
+                img = Image.open(m)
             
-            dateTimeTags = ['DateTimeDigitized','DateTimeOriginal','DateTime']
+                exif_data = img._getexif()
+            
+                if exif_data:
+                    dateTimeTags = ['DateTimeDigitized','DateTimeOriginal','DateTime']
 
-            for dtT in dateTimeTags:
-                datetime = self.getExifField(exif_data,'DateTimeOriginal')
-                if datetime:
-                    break
-                    
+                    for dtT in dateTimeTags:
+                        datetime = self.getExifField(exif_data,'DateTimeOriginal')
+                        if datetime:
+                            break
+                            
+            except Exception as inst:
+                print m
+                pass
+                
             if datetime == None:
                 datetime = time.strftime('%Y:%m:%d', time.gmtime(os.path.getmtime(m)))
-                print(m,' has no EXIF data!',datetime)
+                #print(m,' has no EXIF data!',datetime)
            
             pathDateTime = re.split('[ ,:-]*',datetime)
             
-            pathDateTime = pathDateTime[:3]
+            # use only year/month (for day change to 3)
+            pathDateTime = pathDateTime[:2]
             
             path = '/'.join(pathDateTime)
             
-            print m,path
+            self.path[m] = path 
             
-            
-                        
+
     def printMatches(self):
-        for m in self.matches:
-            print(m)
+        print self.path
+        
+    def copy(self,outputPath):
+        for p in self.path:
+            folder,filename = os.path.split(p)
+            op = os.path.join(outputPath,self.path[p],filename)
+            self.path[p] = op
+            
+    def is_duplicate(self,src,dst):
+        pass
+            
+    def run(self,testrun = True):
+        for p in self.path:
+            folderInput,filenameInput = os.path.split(p)
+            folderOutput,filenameOutout = os.path.split(self.path[p])
+            if testrun:
+                print "mkdir -p ", folderOutput
+                print "copy "
+            else:
+                if not os.path.exists(folderOutput):
+                    os.makedirs(folderOutput)
+                shutil.copy2(p,self.path[p])
+                
 
 imgfolder = ImageFolder();
-imgfolder.scan("/Users/hartmut/Pictures/import/")
-# imgfolder.printMatches()
+imgfolder.scan("/Users/hartmut/Pictures/")
 imgfolder.analyze()
+imgfolder.copy('/Users/hartmut/Pictures/sorted/')
+#imgfolder.printMatches()
+imgfolder.run(True)
